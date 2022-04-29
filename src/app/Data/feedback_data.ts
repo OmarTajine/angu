6547a28bfd388eadbc70c39fdba6f9225ca5adbb
OmarTajine <omartajine1@gmail.com>
2022-04-29 14:20:12 +0100

    commit

--- src/app/Data/feedback_data.ts
+++ src/app/Data/feedback_data.ts
@@ -1,5 +1,5 @@
 export const list = [
     {_id: 1, text: 'hi', rating: 6 },
-    {_id: 2, text: 'hello', rating: 6 },
-    {_id: 3, text: 'hey', rating: 6}
+    {_id: 2, text: 'hello', rating: 5 },
+    {_id: 3, text: 'hey', rating: 2}
   ]
--- src/app/pages/feedback-app/feedback-app.component.ts
+++ src/app/pages/feedback-app/feedback-app.component.ts
@@ -1,5 +1,6 @@
 import { list } from './../../Data/feedback_data';
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-feedback-app',
@@ -13,4 +14,29 @@ export class FeedbackAppComponent implements OnInit {
   ngOnInit(): void {
   }
   items = list
+  text = ''
+  rating = 1
+  nums = [1,2,3,4,5,6,7,8,9,10]
+  add() {
+    if (this.text && this.text.length > 3) {
+      this.items.unshift({ text: this.text, rating :this.rating, _id: 4 })
+      this.text=''
+    }
+  }
+  delete(i: number) {
+    Swal.fire({
+  title: 'Are you sure?',
+  text: "You won't be able to revert this!",
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Yes, delete it!'
+}).then((result) => {
+  if (result.isConfirmed) {
+    this.items.splice(i, 1)
+  }
+}) 
+  }
+  
 }
