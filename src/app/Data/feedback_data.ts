fa34f610523e3c1e3fd888e8d6af35cec58c8167
OmarTajine <omartajine1@gmail.com>
2022-04-29 13:12:11 +0100

    thir commit

---
+++ src/app/Data/feedback_data.ts
@@ -0,0 +1,7 @@
+class Data{
+  lst = [
+    {_id: 1, text: 'hi', rating: 6 },
+    {_id: 2, text: 'hello', rating: 6 },
+    {_id: 3, text: 'hey', rating: 6}
+  ]
+}
--- src/app/pages/feedback-app/feedback-app.component.ts
+++ src/app/pages/feedback-app/feedback-app.component.ts
@@ -11,5 +11,5 @@ export class FeedbackAppComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  
 }
