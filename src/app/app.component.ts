0dab80878d0a7062275832ad5ca3e6977f83cdf6
OmarTajine <omartajine1@gmail.com>
2022-04-29 11:51:16 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-project';
+  age: number = 18;
+  isSelected = false;
+  tab: any[] = [
+    { nom: 'user1', email: 'user1@gmail.com' },
+    { nom: 'user2', email: 'user1@gmai2.com' },
+    { nom: 'user3', email: 'user1@gmai3.com' }
+  ];
+  etat = 1;
+  fn(event : any) {
+    console.log(event)
+    console.log(event.target)
+    console.log(event.target.value)
+    this.title= event.target.value
+  };
+  
 }
